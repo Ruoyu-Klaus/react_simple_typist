024558c12310a474dbac93d4ed8d01dbd8f3e225
RuoyuWang <wangruoyu@dfocuspace.com>
2020-12-28 18:28:27 +0800

    Updates

---
+++ precache-manifest.6eecbbf01f203fdc87ba83120d36b681.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbf309e2c15c75976abceaf5e7d1963",
+    "url": "./index.html"
+  },
+  {
+    "revision": "628afc3185d565129790",
+    "url": "./static/css/main.5cd63c5c.chunk.css"
+  },
+  {
+    "revision": "66f2dea2c03e6b636e65",
+    "url": "./static/js/2.0fcd0d8b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0fcd0d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "628afc3185d565129790",
+    "url": "./static/js/main.8e262090.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.8e262090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfe897e91b7c81fc757",
+    "url": "./static/js/runtime-main.7dcf0ae8.js"
+  }
+]);
