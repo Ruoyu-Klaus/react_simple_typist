695cb3aa794bbfed027cc351927d2d8dd21b9378
RuoyuWang <wangruoyu@dfocuspace.com>
2020-12-28 17:53:21 +0800

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react_typist'
+import 'react_typist/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
